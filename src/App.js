bb0630eea397463761966bfc6de7b5e135d3310d
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2024-09-07 00:00:54 +0100

    label updated

--- src/App.js
+++ src/App.js
@@ -235,7 +235,7 @@ function FormSplitBill({ selectedFriendProps, onSplitBillProps }) {
       value={paidByFriend}
     />
 
-    <label>🤑 Who is paying the bill/label>
+    <label>🤑 Who is paying the bill </label>
     <select
       value={whoIsPaying}
       onChange={(e) => setWhoIsPaying(e.target.value)}
